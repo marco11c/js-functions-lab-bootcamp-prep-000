1245f2460a1256fbfbdc4f0417e0a151a640e278
cayetanomarco@yahoo.com <cayetanomarco@yahoo.com>
2018-06-29 08:34:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function happyHolidaysTo(name) {
 function happyHolidayTo(holiday, name){
   return `Happy ${holiday}, ${name}!`;
 }
-function holidayCountdown(days, holiday){
+function holidayCountdown(holiday, holiday){
   return `It's ${days} until ${holiday}!`;
 }
